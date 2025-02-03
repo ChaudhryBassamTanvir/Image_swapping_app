70370c11e5a0a8b0737ce2fa1bd3afbb367e4e36
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-02-03 12:53:40 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import type {PropsWithChildren} from 'react';
 
 const App = () => {
   return (
@@ -11,4 +12,27 @@ const App = () => {
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceDirection: {
+    margin: 12,
+  },
+  diceImage: {width: 200, height: 200},
+
+  rollDiceBtnTxt: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7EA',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+});
